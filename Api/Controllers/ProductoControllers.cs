afa502b5c7a4acf39c2733b79a37c17705c0d2dd
calebJT7 <calebsingularity@gmail.com>
2026-02-11 11:16:58 -0300

    Completando el CRUD: PUT y DELETE

--- Api/Controllers/ProductoControllers.cs
+++ Api/Controllers/ProductoControllers.cs
@@ -46,5 +46,43 @@ namespace Api.Controllers
 
             return Ok("¡Producto creado con éxito!");
         }
+        //  ACTUALIZAR PRODUCTO (PUT)
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutProducto(int id, ProductoDto productoDto)
+        {
+            // 1. Buscamos si el producto existe en la base de datos
+            var producto = await _context.Productos.FindAsync(id);
+            if (producto == null)
+            {
+                return NotFound("El producto no existe."); // Error 404
+            }
+
+            // 2. Actualizamos solo los datos que nos interesan
+            producto.Nombre = productoDto.Nombre;
+            producto.Precio = productoDto.Precio;
+
+            // 3. Guardamos los cambios en la nube
+            await _context.SaveChangesAsync();
+
+            return Ok("¡Producto actualizado correctamente!");
+        }
+
+        //  ELIMINAR PRODUCTO (DELETE)
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteProducto(int id)
+        {
+            // 1. Buscamos el producto
+            var producto = await _context.Productos.FindAsync(id);
+            if (producto == null)
+            {
+                return NotFound("El producto no existe.");
+            }
+
+            // 2. Lo borramos de la tabla
+            _context.Productos.Remove(producto);
+            await _context.SaveChangesAsync();
+
+            return Ok("¡Producto eliminado para siempre!");
+        }
     }
 }
